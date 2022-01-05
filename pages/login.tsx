efa545a51c59210feaff5da997f9222cd75904bb
Akindeji <akindejioluwaseun015@gmail.com>
2022-01-05 19:53:47 +0100

    update code, init auth pages

---
+++ pages/login.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login | Tabernacle of Psalms</title>
+        <meta name="description" content="Tabernacle of Psalms, Choir Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Login;
---
+++ pages/register.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Register: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Register | Tabernacle of Psalms</title>
+        <meta name="description" content="Tabernacle of Psalms, Choir Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Register;
