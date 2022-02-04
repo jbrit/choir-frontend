30c744624c4803b4049820ad01a8164491e156ca
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-02-05 00:32:54 +0100

    init register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const Register: NextPage = () => {
   return (
@@ -9,6 +12,16 @@ const Register: NextPage = () => {
         <meta name="description" content="Tabernacle of Psalms, Choir Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Grid sx={{minHeight: "100vh"}} container spacing={0}>
+        <Grid item xs={12} md={4}>
+          <Box component='div' sx={{backgroundColor: "primary", width: "100%", minHeight: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            z
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={8}>
+
+        </Grid>
+      </Grid>
     </div>
   );
 };
