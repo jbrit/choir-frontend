4e81004d4b7488de2c8c85ba8dc642c0139895a1
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-04-18 21:18:50 +0100

    chore : abstract modal to a component

---
+++ components/BasicModal.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  maxWidth: 600,
+  width: "90%",
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  px: {xs: 2, md: 6},
+  py: {xs: 2, md: 8}
+};
+
+interface Props {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  children: React.ReactNode;
+  title: string
+}
+
+export default function BasicModal({open, setOpen, children, title} : Props) {
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              {title}
+            </Typography>
+            {children}
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
