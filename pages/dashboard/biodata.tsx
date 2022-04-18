e65b40c8095551df44c9f981101e43bfc81ef0da
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-04-18 17:42:49 +0100

    chore : init biodata page

---
+++ pages/dashboard/biodata.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import React from "react";
+import EditProfileModal from "../../components/EditProfileModal";
+import ResponsiveDrawer from "../../components/ResponsiveDrawer";
+import UserProfileCard from "../../components/UserProfileCard";
+
+const Biodata: NextPage = () => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div>
+      <Head>
+        <title>Biodata | Tabernacle of Psalms</title>
+        <meta name="description" content="Tabernacle of Psalms, Choir Biodata" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ResponsiveDrawer page="Biodata">
+        <UserProfileCard setOpen={setOpen} />
+        <EditProfileModal open={open} setOpen={setOpen} />
+      </ResponsiveDrawer>
+    </div>
+  );
+};
+
+export default Biodata;
+ 
